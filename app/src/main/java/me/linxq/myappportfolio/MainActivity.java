9125a947b5bfcb455a27a84f09350470ac9215b6
Taylor <chartaylorlam@gmail.com>
2016-11-19 00:20:05 +0800

    another method.

--- app/src/main/java/me/linxq/myappportfolio/MainActivity.java
+++ app/src/main/java/me/linxq/myappportfolio/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -21,22 +22,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.btn_app_1:
-                Toast.makeText(this,"启动应用：热门电影", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_app_2:
-                Toast.makeText(this,"启动应用：股票雄鹰", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_app_3:
-                Toast.makeText(this,"启动应用：XYZ阅读器", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_app_4:
-                Toast.makeText(this,"启动应用：最新闻", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_app_5:
-                Toast.makeText(this,"启动应用：毕业设计", Toast.LENGTH_SHORT).show();
-                break;
-        }
+//        vs1:
+//        switch (view.getId()){
+//            case R.id.btn_app_1:
+//                Toast.makeText(this,"启动应用：热门电影", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.btn_app_2:
+//                Toast.makeText(this,"启动应用：股票雄鹰", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.btn_app_3:
+//                Toast.makeText(this,"启动应用：XYZ阅读器", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.btn_app_4:
+//                Toast.makeText(this,"启动应用：最新闻", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.btn_app_5:
+//                Toast.makeText(this,"启动应用：毕业设计", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+
+//      vs2:
+        Button button = (Button)view;
+        String appName = button.getText().toString();
+        Toast.makeText(this, "启动应用：" + appName, Toast.LENGTH_SHORT).show();
     }
 }
