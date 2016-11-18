a7c0e519a8910a42f5d5a42a91ccf7dbf06912e8
Taylor <chartaylorlam@gmail.com>
2016-11-18 13:59:25 +0800

    First commit.

---
+++ app/src/main/java/me/linxq/myappportfolio/MainActivity.java
@@ -0,0 +1,42 @@
+package me.linxq.myappportfolio;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_app_1).setOnClickListener(this);
+        findViewById(R.id.btn_app_2).setOnClickListener(this);
+        findViewById(R.id.btn_app_3).setOnClickListener(this);
+        findViewById(R.id.btn_app_4).setOnClickListener(this);
+        findViewById(R.id.btn_app_5).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_app_1:
+                Toast.makeText(this,"启动应用：热门电影", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_app_2:
+                Toast.makeText(this,"启动应用：股票雄鹰", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_app_3:
+                Toast.makeText(this,"启动应用：XYZ阅读器", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_app_4:
+                Toast.makeText(this,"启动应用：最新闻", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_app_5:
+                Toast.makeText(this,"启动应用：毕业设计", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
